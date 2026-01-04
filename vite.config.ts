5aebbb0f226069348616b0120c49fd1dfdaa6527
Votre Nom <beazibalz@gmail.com>
2026-01-04 20:35:48 +0100

    Update Vite configuration base path for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Pour husvalou.github.io (repo principal). Si sous-repo, mettre '/nom-du-repo/'
+  base: '/secure-account-master/',
 })
 
