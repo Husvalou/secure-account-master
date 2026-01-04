73e6cc4dca515a01fc65a6ccf3d99a095bf3cc53
Votre Nom <beazibalz@gmail.com>
2026-01-04 20:31:06 +0100

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/securing-guide-vite/', // Change this to your repo name
+  base: '/', // Pour husvalou.github.io (repo principal). Si sous-repo, mettre '/nom-du-repo/'
 })
 
